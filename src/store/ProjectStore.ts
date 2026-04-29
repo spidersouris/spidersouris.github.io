38cd3952288ee60f6512bd10cfc5dd8ee740c4f3
IC-EnzoD-FRA <spidersouris@gmail.com>
2026-04-29 17:32:30 +0200

    :fire: chore: remove data-fetching stores (except ProjectStore filter)

--- src/store/ProjectStore.ts
+++ src/store/ProjectStore.ts
@@ -1,48 +1,19 @@
 import { create } from "zustand";
-import { Project } from "../types/project";
-import { getProjects } from "../content/projects";
 import { ProjectStatus } from "../types/project";
 
-interface ProjectStore {
-  projects: Project[];
+interface ProjectFilterStore {
   activeFilters: ProjectStatus[];
-  fetchProjects: () => Promise<void>;
   toggleFilter: (status: ProjectStatus) => void;
-  filteredProjects: () => Project[];
-  featurableProjects: () => Project[];
-  isLoading: boolean;
+  clearFilters: () => void;
 }
 
-export const useProjectStore = create<ProjectStore>((set, get) => ({
-  projects: [],
+export const useProjectFilterStore = create<ProjectFilterStore>((set) => ({
   activeFilters: [],
-  fetchProjects: async () => {
-    const data = await getProjects();
-    set({ projects: data, isLoading: false });
-  },
-  toggleFilter: (status) => {
+  toggleFilter: (status) =>
     set((state) => ({
       activeFilters: state.activeFilters.includes(status)
         ? state.activeFilters.filter((f) => f !== status)
         : [...state.activeFilters, status],
-    }));
-  },
-  filteredProjects: () => {
-    const state = get();
-    return (
-      state.projects
-        .filter((project) =>
-          state.activeFilters.length === 0
-            ? true
-            : state.activeFilters.includes(project.status)
-        )
-        // sort by number of GitHub stars
-        .sort((a, b) => b.githubStats.stars - a.githubStats.stars)
-    );
-  },
-  featurableProjects: () => {
-    const state = get();
-    return state.projects.filter((project) => project.featured);
-  },
-  isLoading: true,
+    })),
+  clearFilters: () => set({ activeFilters: [] }),
 }));
